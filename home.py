e56cc719872acdca62102f73055a0a6f6875d62c
wtran <william.tran955@gmail.com>
2022-03-28 22:27:52 -0700

    ?X

--- home.py
+++ home.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
 myapp_obj = Flask(__name__)
+name = "Lisa"
+city_names = ["Paris", "London", "Rome", "Tahiti"]
 
 @myapp_obj.route("/")
 def home():
-    name = "Lisa"
-    city_names = ["Paris", "London", "Rome", "Tahiti"]
     list_items = ""
     for items in city_names:
         list_items += '<li>' + items + '</li>'
